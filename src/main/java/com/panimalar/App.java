db507e01a3702097f3b8607731603d77f7eced30
G-PRABU <prabu300698@gmail.com>
2021-06-06 17:20:39 +0530

    Initial Commit

---
+++ src/main/java/com/panimalar/App.java
@@ -0,0 +1,13 @@
+package com.panimalar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
